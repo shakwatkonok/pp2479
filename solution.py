fad6bfd01a867789db573b9091928681f2dbde36
Pamelap1997p <76191238+Pamelap1997p@users.noreply.github.com>
2021-02-18 01:54:50 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,34 +5,33 @@ import sys # In order to terminate the program
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
-    #Prepare a sever socket
-    serverSocket.bind(("", port))
+    #Prepare a server socket
+    serverSocket.bind(('', port))  
     #Fill in start
-    serverSocket.bind((”,serverPort))
     serverSocket.listen(1)
-    #print ‘Ready to serve…’
-    connectionSocket, addr = serverSocket.accept
+    #print ‘Ready to serve…’ You can remove this line completley
+    #connectionSocket, addr = serverSocket.accept #remove this line, you are calling the same connection socket in line 20
     #Fill in end
 
     while True:
         #Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr = (13331)
-        serverSocket.accept()
+        connectionSocket, addr = serverSocket.accept() ###()#you dont need the port numbner 13331, the function definition is already setting the port to 13331
+        serverSocket.accept() #move this to the "connectionSocket, addr = " this line goes after the = 
         #Fill in start      #Fill in end
         try:
-            message = ()
+            message = ()#you're missing the rest of the statement here. 
             #Fill in start    #Fill in end
             filename = message.split()[1]
             f = open(filename[1:])
-            outputdata = ()
+            outputdata = () #missing the rest of the statement here
             #Fill in start     #Fill in end
 
             #Send one HTTP header line into socket
             #Fill in start
-            connectionSocket.send('\HTTP/1.1 200 OK\r\n\r\n'.encode())
-            connectionSocket.send(outputdata)
-            connectionSocket.close()
+            
+            connectionSocket.send(outputdata) #notsure if this is needed. 
+            #connectionSocket.close() #you dont need this line right now, you are not closing the connection yet.
 
             #Fill in end
 
@@ -43,26 +42,26 @@ def webServer(port=13331):
             connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
-            pass
             
             #Send response message for file not found (404)
             #Fill in start
 
             #print '404 Not Found'
-            connectionSocket.send('\HTTP/1.1 404 Not Found\n\n')
+            connectionSocket.send('\HTTP/1.1 404 Not Found\n\n'.encode())#need the encode statement here
 
             #Fill in end
 
             #Close client socket
             #Fill in start
-
+			#need to close the socket here
+            connectionSocket.close()
             #Fill in end
             
-         break
-     pass
 
-    #serverSocket.close()
-    #sys.exit()  # Terminate the program after sending the corresponding data
+     serverSocket.close() #Uncomment this out, you need this
+     sys.exit()  # Terminate the program after sending the corresponding data #uncomment ths out, you need this
 
 if __name__ == "__main__":
     webServer(13331)
+
+
