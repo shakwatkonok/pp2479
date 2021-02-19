a7bc1e2d16ab46e3b6b6b61ecbb7b89d803ae841
Pamelap1997p <76191238+Pamelap1997p@users.noreply.github.com>
2021-02-19 01:18:40 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,36 +13,38 @@ def webServer(port=13331):
 
     while True:
         #Establish the connection
-     print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept() ####you dont need the port numbner 13331, the function definition is already setting the port to 13331
-        #serverSocket.accept() #move this to the "connectionSocket, addr = " this line goes after the = 
+     #print('Ready to serve...') need to comment out gradescope doesn’t like print statements 
+        connectionSocket, addr = serverSocket.accept() 
+
+       
         #Fill in start      #Fill in end
         try:
-            message = connectionSocket.recv()
-	    print message,'::',message.split()[0],':',message.split()[1] #you're missing the rest of the statement here. 
+           message = connectionSocket.recv() #need to enter the maximum size of data received at once for the parameter 1024
+	    #print message,'::',message.split()[0],':',message.split()[1] -not sure what this is but you don’t need it
             #Fill in start    #Fill in end
-            filename = message.split()[1]
-	    print filename,'||',[1:]
+           filename = message.split()[1]
+	    #print filename,'||',[1:] No print statement
             f = open(filename[1:])
             outputdata = f.read()
-	    #missing the rest of the statement here
+	
             #Fill in start
-	    print outputdata
+	    #print outputdata
             #Fill in end
 
             #Send one HTTP header line into socket
             #Fill in start
-            
-            #connectionSocket.send(outputdata) #notsure if this is needed. 
-            #connectionSocket.close() #you dont need this line right now, you are not closing the connection yet.
+            #Need to send a http header
+             connectionSocket.send('HTTP/1.0 200 OK\r\n'.encode())
+            #connectionSocket.send(outputdata) #notsure if this is needed.  You need to write out the header see above.
 
             #Fill in end
 
             #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i])
+            connectionSocket.send(outputdata[i]) #needs to be encoded see above for how to encode
 
-            #connectionSocket.send("\r\n".encode())
+            #connectionSocket.send("\r\n".encode())#this is needed dont comment out
+
             connectionSocket.close()
         except IOError:
             
@@ -56,14 +58,15 @@ def webServer(port=13331):
 
             #Close client socket
             #Fill in start
-			#need to close the socket here
+			
             connectionSocket.close()
             #Fill in end
             
 
-       serverSocket.close()
-       sys.exit()  # Terminate the program after sending the corresponding data
+     serverSocket.close()
+     sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
      webServer(13331) 
+
 
