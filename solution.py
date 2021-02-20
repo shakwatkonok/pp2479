b26d5610519b038725f21b513c3cb4bb5601e36c
Pamelap1997p <76191238+Pamelap1997p@users.noreply.github.com>
2021-02-19 19:57:56 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,68 @@
+# import socket module
+from socket import *
+import  sys  # In order to terminate the pr
+
+
+def webServer(port=13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    # Prepare a server socket
+    serverSocket.bind(("", port))
+    # Fill in start
+    serverSocket.listen(1)
+    # Fill in end
+
+    while True:
+        # Establish the connection
+        #print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()
+
+
+        try:
+            message = connectionSocket.recv(1024)
+            # Fill in start    #Fill in end
+            filename = message.split()[1]
+            f = open(filename[1:])
+            outputdata = f.read()
+
+
+
+            # Send one HTTP header line into socket
+            # Fill in start
+            # Need to send a http header
+            connectionSocket.sendall('HTTP/1.0 200 OK\r\n'.encode())
+        # connectionSocket.send(outputdata)
+
+        # Fill in end
+
+        # Send the content of the requested file to the client
+
+            for i in range(0, len(outputdata)):
+
+             connectionSocket.sendall(outputdata[i].encode())
+
+
+             connectionSocket.send("\r\n".encode())
+
+             connectionSocket.close()
+
+        except IOError:
+
+# Send response message for file not found (404)
+# Fill in start
+
+# print '404 Not Found'
+             connectionSocket.sendall('HTTP/1.0 404 Not Found\r\n'.encode())
+
+# Fill in end
+
+# Close client socket
+# Fill in start
+             connectionSocket.close()
+# Fill in end
+        serverSocket.close()
+        sys.exit()  # Terminate the program after sending the corresponding data
+
+    if __name__ == "__main__":
+        webServer(13331)
+
