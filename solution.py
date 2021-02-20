717ce40b7ddf4fedfcd90f9a5cec4da51aaec5b5
Pamelap1997p <76191238+Pamelap1997p@users.noreply.github.com>
2021-02-20 12:49:08 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -31,7 +31,7 @@ def webServer(port=13331):
 
             # Send the content of the requested file to the client
             
-        for i in range(0, len(outputdata)):
+            for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
 
             connectionSocket.send("\r\n".encode())
