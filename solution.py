c3913f5cf73abdffad948180c7a9ea3d4fb06768
Pamelap1997p <76191238+Pamelap1997p@users.noreply.github.com>
2021-04-19 21:52:48 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import struct
 import time
 import select
 import binascii
-sudo 'import ICMPPinger; ICMPPinger.ping("<URL or IP>", timeout=<time in sec>
 
+sudo import ICMPPinger; ICMPPinger.ping("<URL or IP>", timeout=<time in sec>)
 #sudo python ICMPPinger.py
 
 label = '*************{0}*************'
